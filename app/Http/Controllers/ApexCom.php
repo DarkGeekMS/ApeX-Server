cc5ecc6e715b8f6575f9697e7c7d885bc84d9801
KareemOsamaSobeih <37814454+KareemOsamaSobeih@users.noreply.github.com>
2019-03-08 02:50:05 +0200

    Update ApexCom.php

--- app/Http/Controllers/ApexCom.php
+++ app/Http/Controllers/ApexCom.php
@@ -8,11 +8,13 @@ class ApexCom extends Controller
 {
 
   /**
-* @bodyParam title string required The title of the post.
-* @bodyParam body string required The title of the post.
-* @bodyParam type string The type of post to create. Defaults to 'textophonious'.
-* @bodyParam author_id int the ID of the author
-* @bodyParam thumbnail image This is required if the post type is 'imagelicious'.
+   * @bodyParam ApexCom_id string required The fullname of the community.
+   * @bodyParam _token string required The token required for authentication.
+   * Success Cases :
+   * 1) return the information about the ApexCom.
+   * failure Cases:
+   * 1) NoAccessRight the token does not support to view the about information.
+   * 2) ApexCom fullname (ApexCom_id) is not found.
 */
 
   public function About()
@@ -21,11 +23,19 @@ class ApexCom extends Controller
 
 
   /**
-* @bodyParam title string required The title of the post.
-* @bodyParam body string required The title of the post.
-* @bodyParam type string The type of post to create. Defaults to 'textophonious'.
-* @bodyParam author_id int the ID of the author
-* @bodyParam thumbnail image This is required if the post type is 'imagelicious'.
+   * @bodyParam ApexCom_id string required The fullname of the community where the post is posted.
+   * @bodyParam body string required The text body of the post.
+   * @bodyParam image_file file The attached image to the post.
+   * @bodyParam image_type string The type of the attached image to the post(png is default, jpg).
+   * @bodyParam video_url string The url to attached video to the post.
+   * @bodyParam link string The link attached to the post.
+   * @bodyParam _token string required The token required for authentication.
+   * Success Cases :
+   * 1) return true to ensure that the post was added to the ApexCom successfully.
+   * failure Cases:
+   * 1) NoAccessRight the token does not support to Create a post in this ApexCom.
+   * 2) ApexCom fullname (ApexCom_id) is not found.
+   * 3) The content of the post is not sufficient.
 */
 
   public function Posts()
@@ -33,13 +43,17 @@ class ApexCom extends Controller
 
 
 
+ 
   /**
-* @bodyParam title string required The title of the post.
-* @bodyParam body string required The title of the post.
-* @bodyParam type string The type of post to create. Defaults to 'textophonious'.
-* @bodyParam author_id int the ID of the author
-* @bodyParam thumbnail image This is required if the post type is 'imagelicious'.
+   * @bodyParam ApexCom_id string required The fullname of the community required to be subscribed.
+   * @bodyParam _token string required The token required for authentication.
+   * Success Cases :
+   * 1) return true to ensure that the subscription or unsubscribtion has been done successfully.
+   * failure Cases:
+   * 1) NoAccessRight the token does not support to subscribe this ApexCom.
+   * 2) ApexCom fullname (ApexCom_id) is not found.
 */
+
 
   public function Subscribe()
   {return;}
@@ -47,11 +61,17 @@ class ApexCom extends Controller
 
 
   /**
-* @bodyParam title string required The title of the post.
-* @bodyParam body string required The title of the post.
-* @bodyParam type string The type of post to create. Defaults to 'textophonious'.
-* @bodyParam author_id int the ID of the author
-* @bodyParam thumbnail image This is required if the post type is 'imagelicious'.
+   * @bodyParam ApexCom_name string required The name of the community.
+   * @bodyParam description string required The description of the community.
+   * @bodyParam type string required The type of the community(there are only three valid types public, restricted, private).
+   * @bodyParam header_image_file file required The header image.
+   * @bodyParam header_image_type string required The type of the header image (png is default, jpg).
+   * @bodyParam _token string required The token required for authentication.
+   * Success Cases :
+   * 1) return true to ensure that the ApexCom was created or edited successfully.
+   * failure Cases:
+   * 1) NoAccessRight the token does not support to Create or to edit an ApexCom.
+   * 2) Wrong or unsufficient submitted information.
 */
 
   public function Admin()
