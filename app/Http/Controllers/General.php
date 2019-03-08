bcfb71c8f89e6f6e1b3339b00dc82eb3d59107b1
Muhammad Sayed <hamada.sayed2010@gmail.com>
2019-03-08 16:11:51 +0200

    Add successs and failure cases

--- app/Http/Controllers/General.php
+++ app/Http/Controllers/General.php
@@ -10,8 +10,12 @@ class General extends Controller
     /**
      * Search
      * Returns a list of lists of communities, posts and profiles that matches the given query
-  * @bodyParam query string required The query to be searched for.
-  */
+    * @bodyParam query string required The query to be searched for.
+    * Success Cases :
+    * 1) Return the result successfully.
+    * failure Cases:
+    * 1) No matches found
+    */
 
     public function Search()
     {return;}
@@ -19,11 +23,16 @@ class General extends Controller
 
 
       /**
-       * SortPostsBy
-       * Returns a list of posts in a given ApexComm sorted either by the votes or by the date 
+      * SortPostsBy
+      * Returns a list of posts in a given ApexComm sorted either by the votes or by the date 
     * @bodyParam ApexCommID string required The ID of the ApexComm that contains the posts.
-    * @bodyParam SortingParam string required The sorting parameter, takes a value of ['votes', 'date'].
+    * @bodyParam SortingParam string The sorting parameter, takes a value of ['votes', 'date'], Default is 'date'.
     * @bodyParam desc bool To specify wether the sort is descending or ascending, Default is true
+    * Success Cases :
+    * 1) Return the result successfully.
+    * failure Cases:
+    * 1) ApexComm fullname (ID) is not found.
+    * 2) The given parameter is out of the specified values, in this case it uses the default values
     */
 
       public function SortPostsBy()
@@ -32,9 +41,13 @@ class General extends Controller
 
 
         /**
-         * ApexNames
-         * Returns a list of the names of the existing ApexComms
-      */
+        * ApexNames
+        * Returns a list of the names of the existing ApexComms
+    * Success Cases :
+    * 1) Return the result successfully.
+    * failure Cases:
+    * 1) Return empty list if there are no existing ApexComms
+    */
 
         public function ApexNames()
         {return;}
--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -18,6 +18,11 @@ class User extends Controller
    * Block a user, so he can't send private messages to the current user
 * @bodyParam id string required the id of the user to be blocked.
 * @bodyParam token JWT required Used to verify the user.
+* Success Cases :
+* 1) Return true to ensure that the user has been blocked successfully.
+* failure Cases:
+* 1) NoAccessRight the token is not for the user that wants to block the other user
+* 2) User fullname (ID) is not found.
 */
 
   public function Block()
@@ -32,6 +37,12 @@ class User extends Controller
 * @bodyParam subject string required The subject of the message.
 * @bodyParam mes text the body of the message.
 * @bodyParam token JWT required Used to verify the user.
+* Success Cases :
+* 1) return true to ensure that the message has been sent successfully.
+* failure Cases:
+* 1) NoAccessRight the token is not for the user that wants to compose a message
+* 2) The user is blocked and can't be messaged
+* 3) User fullname (ID) is not found.
 */
 
   public function Compose()
@@ -43,7 +54,10 @@ class User extends Controller
    * UserDataByAccountID
    * Return user public data to be seen by another user
 * @bodyParam id string required The id of an existing user.
-* @bodyParam token JWT required Used to verify the user.
+* Success Cases :
+* 1) return the data of the user successfully.
+* failure Cases:
+* 1) User fullname (ID) is not found.
 */
 
   public function UserDataByAccountID()
