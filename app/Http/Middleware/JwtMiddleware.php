3c89fc4d99959734598ebfbce46212178d4debb3
Mohamed Ramzy <mramzytechioneer@gmail.com>
2019-03-25 21:30:59 +0200

    Added jwtMiddleware

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Not authorized']);
+        }
+        return $next($request);
+    }
+}
