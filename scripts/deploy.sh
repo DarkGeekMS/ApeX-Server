c88a83fa1b2b365c1af18a0364734d0a0ba2ce94
Mahmoud Adas <mido3ds@gmail.com>
2019-04-13 00:02:08 +0200

    prdouction-docker-compose.yml is part of repo and get copied to vm every deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,22 @@ COMPUTE_ZONE="us-central1-c"
 VM_INSTANCE="mido3ds@main"
 
 main() {
-    gcloud auth activate-service-account $GCD_ACCOUNT_SECRET  --key-file="$GCD_KEYFILE_SECRET"
+    # login
+    gcloud auth activate-service-account \
+	$GCD_ACCOUNT_SECRET  --key-file="$GCD_KEYFILE_SECRET"
     
-    gcloud compute ssh $VM_INSTANCE --project $PROJECT_ID --zone $COMPUTE_ZONE --command="sudo BRANCH=${BRANCH:master} docker-compose up --build --no-deps -d"
+    gcloud config set project $PROJECT_ID
+    gcloud config set compute/zone $COMPUTE_ZONE
+
+    # copy docker-compose.yml to update it there
+    gcloud compute scp \
+	production-docker-compose.yml \
+	$VM_INSTANCE:docker-compose.yml 
+
+    # run docker-compose
+    gcloud compute ssh $VM_INSTANCE \
+	--command="sudo BRANCH=${BRANCH:master} \
+		   docker-compose up --build --no-deps -d"
 }
 
 if [[ "$#" == 2 ]]; then
