a6bc4cc10099c29cf3d779cd6cc10ce58b4b1379
Mahmoud Adas <mido3ds@gmail.com>
2019-04-12 23:04:27 +0200

    docker-compose has environment variables

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,4 @@ set -ex
 phpenv config-rm xdebug.ini # remove xdebug to speed up testing
 cp .env.example .env
 
-docker build . -t app-image --build-arg MIGRATE='true' --build-arg TEST='true'
-docker-compose up --exit-code-from app
+env PORT=8080 MIGRATE='true' TEST='true' docker-compose up --build --exit-code-from app
