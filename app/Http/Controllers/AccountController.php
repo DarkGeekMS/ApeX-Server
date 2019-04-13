75b53d9dd12b132df71977abc3831122b05b32d4
Remonda Talaat <44608184+RemondaTalaat@users.noreply.github.com>
2019-04-13 20:45:26 +0200

    [Uupdate] function doc and unit testing for get apexComs request

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -369,8 +369,7 @@ class AccountController extends Controller
         }
         $msgid= $request['ID'];
         $msgCheck=DB::table('messages')->where('id', '=', $msgid)->get();
-        if(count($msgCheck)==0)
-        {
+        if (count($msgCheck)==0) {
             return response()->json(['error' => 'Message doesnot exist'], 500);
         }
         $subject=DB::table('messages')->where('id', '=', $msgid)->select('subject')->get();
@@ -378,14 +377,11 @@ class AccountController extends Controller
             ->where('messages.id', '=', $msgid)
             ->orWhere('messages.parent', $msgid)
             ->orderBy('messages.created_at', 'asc')
-            ->select('username','content','messages.created_at')
+            ->select('username', 'content', 'messages.created_at')
             ->get();
-    
-       $json_output=response()->json(['message' =>$msg ,'subject'=>$subject ]);
+        $json_output=response()->json(['message' =>$msg ,'subject'=>$subject ]);
        //$json_output = json_encode( [$msg,$subject] );
         return $json_output;
-
-
     }
 
 
@@ -512,10 +508,10 @@ class AccountController extends Controller
         $savedposts=DB::table('save_posts')->join('posts', 'save_posts.postID', '=', 'posts.id')->where('posts.posted_by', '=', $id)->select('content')->get();
         $hiddenposts=DB::table('hiddens')->join('posts', 'hiddens.postID', '=', 'posts.id')->where('posts.posted_by', '=', $id)->select('content')->get();
         $apexcom=DB::table('moderators')->join('apex_coms', 'moderators.apexID', '=', 'apex_coms.id')->where('moderators.userID', '=', $id)->select('name','description')->get();
-        
-        
+
+
         if($type == 2)
-            $json_output=response()->json(['user_info' =>$info ,'posts'=>$posts ,'saved_posts'=>$savedposts ,'hidden_posts'=>$hiddenposts ,'apex_coms'=>$apexcom  ]);  
+            $json_output=response()->json(['user_info' =>$info ,'posts'=>$posts ,'saved_posts'=>$savedposts ,'hidden_posts'=>$hiddenposts ,'apex_coms'=>$apexcom  ]);
         else
             $json_output=response()->json(['user_info' =>$info ,'posts'=>$posts ,'saved_posts'=>$savedposts ,'hidden_posts'=>$hiddenposts ]);
         return $json_output;
