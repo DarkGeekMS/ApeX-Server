2782d4e308616d43d5bdb6d70f6738737005c631
Mahmoud Adas <mido3ds@gmail.com>
2019-04-14 08:44:50 +0000

    e2e on production server

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,6 +10,8 @@ env PORT=8080 MIGRATE='true' TEST='true' docker-compose up --build --exit-code-f
 # e2e tests
 #env PORT='127.0.0.1:8080' WEB_BRANCH=master docker-compose up --build -d
 git clone https://${GITHUB_TOKEN}@github.com/RehamGamal97/apeXTesting e2e
+docker build e2e -t e2e --build-arg SERVER_URL="http://35.232.3.8" 
+docker run --rm -it e2e 
 #docker run -it --rm -v "$(pwd)"/e2e/:/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 chmod +x /usr/src/mymaven/driver/chromedriver.exe
 #docker run -it --rm -v "$(pwd)"/e2e/:/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install
-env PORT=8080 docker-compose -f e2e-docker-compose.yml up --build --exit-code-from e2e
+#env PORT=8080 docker-compose -f e2e-docker-compose.yml up --build --exit-code-from e2e
