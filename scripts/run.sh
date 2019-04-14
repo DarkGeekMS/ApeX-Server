683450cd8f4e5e6ff9e110d8a642d0358d68fdd1
Mahmoud Adas <mido3ds@gmail.com>
2019-04-14 21:58:57 +0200

    [skip ci] added TA TODOs for Devops

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,19 +3,20 @@ set -ex
 
 composer install -o
 npm install
+php7 artisan migrate -v -n
+composer dump-autoload
+
+#TODO rename MIGRATE to SEED
 
 if [[ "$MIGRATE" == 'true' ]]; then 
-    php7 artisan migrate -v
-    composer dump-autoload
     php7 artisan DB:seed
     php7 artisan jwt:secret
-
 fi
 
 
 runTests() { ./vendor/bin/phpunit --bootstrap ./vendor/autoload.php --testdox tests; }
 serve() { php7 artisan serve --host=0.0.0.0 --port=80; }
-
+#TODO run serve in production
 if [[ "$TEST" == 'true' ]]; then
     serve &
     sleep 3
