2ede5eaac9342e7a40258cef1df2e19bec122d30
Mahmoud Adas <mido3ds@gmail.com>
2019-04-22 15:52:01 +0000

    added e2e to run.sh, Dockerfile, ci.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,15 @@ migrate() {
 
 seed() { php7 artisan DB:seed -n --force; }
 
-runTests() { ./vendor/bin/phpunit --bootstrap ./vendor/autoload.php --testdox tests; }
+unitTests() { ./vendor/bin/phpunit --bootstrap ./vendor/autoload.php --testdox tests; }
+
+e2eTests() { 
+    git clone https://${GITHUB_TOKEN}@github.com/RehamGamal97/apeXTesting e2e
+    pushd e2e
+      cat scripts/dependencies | xargs apk add
+      ./sripts/run.sh "http://localhost:80"
+    popd
+}
 
 serve() { php7 artisan serve --host=0.0.0.0 --port=80; }
 
@@ -24,10 +32,17 @@ if [[ "$SEED" == 'true' ]]; then
     seed
 fi
 
-if [[ "$TEST" == 'true' ]]; then
+if [[ "$UNIT_TEST" == 'true' ]] || [[ "$E2E_TEST" == 'true' ]]; then
     serve &
-    sleep 3
-    runTests
+    sleep 10
 else
     serve
+fi
+
+if [[ "$UNIT_TEST" == 'true' ]]; then 
+    unitTests
+fi
+
+if [[ "$E2E_TEST" == 'true' ]]; then
+    e2eTests
 fi
