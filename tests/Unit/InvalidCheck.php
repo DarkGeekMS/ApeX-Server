c3496fb528d4b35da0ab91c461ea0188fabe6a5f
Remonda Talaat <44608184+RemondaTalaat@users.noreply.github.com>
2019-04-26 20:34:26 +0200

    unit testing updates

---
+++ tests/Unit/InvalidCheck.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InvalidCheck extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->json(
+            'POST',
+            '/api/check_code',
+            [
+            'username' => 'MohamedRamzy',
+            'code' => 'bla bla bla'
+            ]
+        );
+        $response->assertStatus(400);
+    }
+}
