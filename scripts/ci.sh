ae1b9968f6de3dae5a0b3f94c5370ea957c494bb
Mahmoud Adas <mido3ds@gmail.com>
2019-04-27 18:24:25 +0000

    fixed github token issue with travis

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,7 @@ set -ex
 phpenv config-rm xdebug.ini # remove xdebug to speed up testing
 cp .env.example .env
 
-env GITHUB_TOKEN=$GITHUB_TOKEN PORT=8080 SEED='true' UNIT_TEST='true' E2E_TEST='true'  docker-compose up --build --exit-code-from app
+echo GITHUB_TOKEN=$GITHUB_TOKEN >> .env
+
+source .env && env PORT=8080 SEED='true' UNIT_TEST='true' E2E_TEST='true'  docker-compose up --build --exit-code-from app
 
